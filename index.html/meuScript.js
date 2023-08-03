b8ed49340f473c6ac3d1eadcf480642c3c2b788f
ArtleandroMDM <138249836+ArtleandroMDM@users.noreply.github.com>
2023-08-03 16:54:54 -0300

    upPortifoliocss

---
+++ index.html/meuScript.js
@@ -0,0 +1,17 @@
+alert("Ola Mundo estou aprendendo javaScript")
+document.getElementById('nome').value = "oi"
+   //string
+   var texto = "javaScript"
+
+   //number 
+   var numeroInteiro = 10
+   var numeroFracionado = 11210.75
+
+   //boolan 
+   var teste = true 
+
+   alert(numeroFracionado)
+
+   document.write(texto)
+
+   console.log(numeroInteiro)
