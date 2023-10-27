4274d436464846deb839180721af0300e62d674d
ArtleandroMDM <138249836+ArtleandroMDM@users.noreply.github.com>
2023-10-27 16:50:40 -0300

    Class
    Classe pessoa criada evolucao do app

--- Mobile/Applistaalunos/app/src/main/java/defandroide/arthur/applistaaluno/Model/Pessoa.java
+++ Mobile/Applistaalunos/app/src/main/java/defandroide/arthur/applistaaluno/Model/Pessoa.java
@@ -1,4 +1,55 @@
 package defandroide.arthur.applistaaluno.Model;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 public class Pessoa {
+
+    private String primeiroNome;
+    private String SobreNome;
+    private String Matricula;
+    private String cpf;
+
+    public Pessoa(){}
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String PrimeiroNome) {
+        primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return SobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.SobreNome = sobreNome;
+    }
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        Matricula = matricula;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "primeiroNome='" + primeiroNome + '\'' +
+                ", SobreNome='" + SobreNome + '\'' +
+                ", Matricula='" + Matricula + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
 }
--- Mobile/Applistaalunos/app/src/main/java/defandroide/arthur/applistaaluno/View/MainActivity.java
+++ Mobile/Applistaalunos/app/src/main/java/defandroide/arthur/applistaaluno/View/MainActivity.java
@@ -8,11 +8,20 @@ import defandroide.arthur.applistaaluno.Model.Pessoa;
 import defandroide.arthur.applistaaluno.R;
 
 public class MainActivity extends AppCompatActivity {
-    Pessoa
+
+    Pessoa pessoa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pessoa = new Pessoa();
+
+        pessoa.setPrimeiroNome("Super-Man");
+        pessoa.setSobreNome("GamerGrato_Pro34");
+        pessoa.setMatricula("010101");
+        pessoa.setCpf("123.546.744-01");
+
     }
 }
