07d0e71ffdc45ade1a48600e8d56436890468558
ArtleandroMDM <138249836+ArtleandroMDM@users.noreply.github.com>
2023-10-31 16:52:24 -0300

    AjustiDosBotoes
    uma melhora nos botao com uma tentativa para já fazer funcionar

--- Mobile/MyAVA20/app/src/main/java/View/MainActivity.java
+++ Mobile/MyAVA20/app/src/main/java/View/MainActivity.java
@@ -1,14 +1,21 @@
-package defandroide.arthur.myava20;
+package View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import Controller.CursoController;
+import Model.Curso;
+import Model.Pessoa;
+import defandroide.arthur.myava20.R;
+
 public class MainActivity extends AppCompatActivity {
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pessoa = new Pessoa();
     }
 }
