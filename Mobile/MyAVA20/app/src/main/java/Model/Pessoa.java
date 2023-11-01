e8fdf48eb80429551566c9dba3c49afd210a7c6c
ArtleandroMDM <138249836+ArtleandroMDM@users.noreply.github.com>
2023-11-01 16:54:52 -0300

    MelhoradoBotaoPt2
    continuação do botão do dia anterior e ajuste do celular que não estava funcio -
    nado #Pt2 e ultima Finalizada. ;-;

--- Mobile/MyAVA20/app/src/main/java/Model/Pessoa.java
+++ Mobile/MyAVA20/app/src/main/java/Model/Pessoa.java
@@ -1,7 +1,10 @@
 package Model;
 
 public class Pessoa {
-
+    private String idcalculo;
+    public void setidcalculo(String idcalculo) {
+        idcalculo = idcalculo;
+    }
 
     public Pessoa(){}
 }
--- Mobile/MyAVA20/app/src/main/java/View/MainActivity.java
+++ Mobile/MyAVA20/app/src/main/java/View/MainActivity.java
@@ -3,6 +3,7 @@ package View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import Controller.CursoController;
 import Model.Curso;
@@ -12,10 +13,40 @@ import defandroide.arthur.myava20.R;
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
 
+
+
+    Button btnNum1;
+    Button btnNum2;
+    Button btnNum3;
+    Button btnNum4;
+    Button btnNum5;
+    Button btnNum6;
+    Button btnNum7;
+    Button btnNum8;
+    Button btnNum9;
+    Button btnNum0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pessoa = new Pessoa();
+        pessoa.setidcalculo("Super-Man");
+
+        btnNum1 = findViewById(R.id.idNm1);
+        btnNum1 = findViewById(R.id.idNm2);
+        btnNum1 = findViewById(R.id.idNm3);
+        btnNum1 = findViewById(R.id.idNm4);
+        btnNum1 = findViewById(R.id.idNm5);
+        btnNum1 = findViewById(R.id.idNm6);
+        btnNum1 = findViewById(R.id.idNm7);
+        btnNum1 = findViewById(R.id.idNm8);
+        btnNum1 = findViewById(R.id.idNm9);
+        btnNum1 = findViewById(R.id.idNm0);
+
+
+
+
     }
 }
